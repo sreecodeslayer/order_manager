7276c473492aa631aaff4be25b18f4628b44951d
Sreenadh TC <kesav.tc8@gmail.com>
2018-04-30 22:17:09 +0530

    Add orders model

---
+++ order_manager/models/orders.py
@@ -0,0 +1,21 @@
+from order_manager.extensions import db, pwd_context
+from datetime import datetime, timedelta
+
+from .users import Users
+from .crops import Crops
+
+
+class Orders(db.Document):
+    '''Mongodb Model for Orders'''
+    ordered_by = db.ReferenceField(Users)
+    items = db.ListField(db.ReferenceField(Crops))
+    total = db.FloatField(default=0)
+    ordered_on = db.DateTimeField(
+        default=datetime.utcnow() + timedelta(hours=5, minutes=30))
+
+    meta = {
+        'indexes': ['ordered_on']
+    }
+
+    def __init__(self, **kwargs):
+        super(Orders, self).__init__(**kwargs)
